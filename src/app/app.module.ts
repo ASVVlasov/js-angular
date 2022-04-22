a4fd374d4ca90a17c09a22bb90bf71b6079ac058
Aleksandr Vlasov <asvvlasov@gmail.com>
2022-04-22 18:28:31 +0300

    feat: add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatDatepickerModule, MatButtonToggleModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
